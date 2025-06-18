e74b18062e5550934ee6cdcbf7fdc99a23cdf77c
MUGHILAN_OFFICIAL <mca211117@smvec.ac.in>
2025-06-18 12:55:19 +0530

    Completed

---
+++ SimpleCalculator/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Class1
+    {
+
+        public void add() 
+        {           
+                Console.WriteLine("=========== ADD =========");
+                Console.Write("A: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("B: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(a + b);
+
+        }
+
+        public void sub() 
+        {
+            Console.WriteLine("=========== SUB =========");
+            Console.Write("A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+                long c = a - b; Console.WriteLine("Answer: "+ c);
+        }
+
+
+        public void mul() {
+            Console.WriteLine("=========== mul =========");
+            Console.Write("A: ");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B: ");
+            float b = Convert.ToInt32(Console.ReadLine());
+          
+                double c = a * b; Console.WriteLine("Answer: "+ c);
+
+        }
+
+
+        public void div()
+        {
+            Console.WriteLine("=========== DIV =========");
+            Console.Write("A: ");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B: ");
+            float b = Convert.ToInt32(Console.ReadLine());
+            
+                double c = a / b; Console.WriteLine("Answer: "+ c);
+          }
+
+        public void mod()
+        {
+            Console.WriteLine("=========== Mod =========");
+            Console.Write("A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B: ");
+           int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a%b);
+        }
+
+
+        public void greaterSmall()
+        {
+            Console.WriteLine("=========== Positive or Negative =========");
+            Console.Write("Enter number Positive or Negative: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(num >= 0 ? "Number is Positive" : "Number is Negative");
+
+        }
+
+
+        public void greaterLarge() {
+            Console.WriteLine("=========== Greatest or smallest =========");
+            Console.Write("First number: ");
+            long a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Secound Number: ");
+            long b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a > b ? "First Value is greater" : "Secound value is greater");
+        }
+
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SimpleCalculator;
+using System.ComponentModel;
+
+namespace simpleCalculator
+{ 
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Class1 myObj = new Class1();
+            Console.WriteLine("simple calculator");
+
+            while (true) {
+                Console.WriteLine("1.For add \n2.Sub \n3.multply \n4.Div \n5.Modules \n6.Greatest or smallest \n7.Numbers are positive");
+                int switch_value = Convert.ToInt32(Console.ReadLine());
+                switch (switch_value)
+                {
+                    case 1:
+                        myObj.add();          
+                     break;
+                    
+                    case 2:
+                        myObj.sub();
+                        break;
+
+                    case 3:
+                        myObj.mul();
+                        break;
+
+                    case 4:
+                        myObj.div();
+                        break;
+
+                    case 5:
+                        myObj.mod();
+                        break;
+
+                    case 6:
+                        myObj.greaterLarge();
+                        break;
+                    case 7:
+                        myObj.greaterSmall();
+                        break;
+
+                    default:
+                        Console.WriteLine("break");
+                        break;
+                        
+                }
+
+                Console.WriteLine("Sure want to exit..! Y/n");
+                string input = Console.ReadLine().ToUpper();
+                if (input != "N") {
+                    break;
+                }
+            }
+        }
+    }
+}
